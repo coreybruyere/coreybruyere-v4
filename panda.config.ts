cfb94df604f654bde5c37d0e3f5a69994f77ac7b
Corey Bruyere <coreybruyere@ymail.com>
2024-04-03 09:11:02 -0700

    feat: Initial pandaCSS setup

--- panda.config.ts
+++ panda.config.ts
@@ -5,14 +5,34 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: ['./src/**/*.{ts,tsx,js,jsx,astro}', './pages/**/*.{ts,tsx,js,jsx,astro}'],
+  include: [
+    "./src/**/*.{ts,tsx,js,jsx,astro}",
+    "./pages/**/*.{ts,tsx,js,jsx,astro}",
+  ],
 
   // Files to exclude
   exclude: [],
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        colors: {
+          primary: { value: "{colors.teal.900}" },
+          secondary: { value: "{colors.orange.900}" },
+        },
+      },
+      semanticTokens: {
+        colors: {
+          danger: {
+            value: { base: "{colors.red.400}", _dark: "{colors.green.900}" },
+          },
+          success: {
+            value: { base: "{colors.green.400}", _dark: "{colors.green.900}" },
+          },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "CB";
+export const SITE_DESCRIPTION =
+  "Corey Bruyere's Portolio and Personal Website!";
