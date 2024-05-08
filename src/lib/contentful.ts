3f9d1191020f7314d49d6bc6eb73b42f352659b5
Corey Bruyere <coreybruyere@ymail.com>
2024-05-07 17:11:18 -0700

    test: accessToken

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -15,8 +15,6 @@ export interface Post {
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV
-    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
-    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  accessToken: import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
   host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
