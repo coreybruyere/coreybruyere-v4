05c5c8e9d41cf400c40f7a895f127a91980d64ab
Corey Bruyere <coreybruyere@ymail.com>
2024-08-09 14:19:37 -0700

    feat: Add general page

--- panda.config.ts
+++ panda.config.ts
@@ -12,6 +12,12 @@ export default defineConfig({
 
   // Files to exclude
   exclude: [],
+  // conditions: {
+  //   light: "[data-color-mode=light] &",
+  //   dark: "[data-color-mode=dark] &",
+  //   pinkTheme: "[data-theme=pink] &",
+  //   blueTheme: "[data-theme=blue] &",
+  // },
 
   // Useful for theme customization
   theme: {
@@ -29,6 +35,18 @@ export default defineConfig({
           },
           success: {
             value: { base: "{colors.green.400}", _dark: "{colors.green.900}" },
+          },
+          text: {
+            value: {
+              _pinkTheme: {
+                base: "{colors.pink.500}",
+                _dark: "{colors.pink.300}",
+              },
+              _blueTheme: {
+                base: "{colors.blue.500}",
+                _dark: "{colors.blue.300}",
+              },
+            },
           },
         },
       },
